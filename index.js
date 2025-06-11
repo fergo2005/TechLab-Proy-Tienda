dfde50d16e2812077b96246f43261d4c7240531e
FSGomez <fergo2005@gmail.com>
2025-06-11 16:39:13 -0300

    2025-06-11-actualizado

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-console.log("Index.js activo");
+console.log("Index.js está activo");
+
+
+const argumentos=process.argv.slice(2);
+
+const [comando, ...args] =argumentos;
+
+switch (comando) {
+    case "GET": 
+        console.log("Todos los productos", args[0]);        
+        break;
+    case "POST": 
+        console.log('Agregamos el siguiente producto: title:', args[1], 'price:', args[2], 'category:', args[3],);        
+        break;
+    case "PUT": 
+        console.log('Modificamos el item con id:', args[0], 'satisfactoriamente.');        
+        break;
+    case "DELETE": 
+        console.log('El item con id:', args[0], ' se eliminó con éxito.');        
+        break;
+    default: console.log("Comando desconocido...");
+        break;
+}
+
+
+
+
+
+
+
 
 fetch('https://fakestoreapi.com/products')
   .then(response => response.json())
-  .then(data => console.log(data));
+  /* .then(data => console.log(data)); */
+
--- script.js
+++ script.js
@@ -1,3 +1,59 @@
+// Copia de otros solo para ver con tipo Commonjs antigui
+/* const fs=require("fs");
+const os = require("os");
+const math = require("./modulos/math");
+const {suma, resta, multi, divi} = require("./modulos/math"); */
+
+//desde el archivo donde estan los modulos se exporta asi
+/* module.exports = {suma: suma, resta: resta, multi: multi, divi: divi };  */
+
+
+console.log(math);
+console.log(suma(2,4));
+console.log(resta(2,4));
+console.log(multi(2,4));
+console.log(divi(2,4));
+
+
+
+//modulos modernos 
+import fs from 'fs'
+import {suma, resta, multi, divi} from "./modulos/math.js"; //siempre con la extension
+//Tambien cambiar la forma en que se exporta...
+//en la carpeta modulos archivo math.js se le agrega export a cada funcion a exportar
+//ejemplo:
+//export const suma = (a, b)=> a + b; // etc etc
+// o sin el export en cada funcion agrego un export por default como:
+//export default {suma, resta, multi, divi};
+// o
+// terniendo un--- const math = {suma, resta, multi, divi};
+// export default math;
+
+
+
+
+/* console.log(os.platform());
+console.log(os.arch());
+console.log(os.release());
+console.log(os.networkInterfaces()); */
+
+
+/* fs.writeFile("./archivo.txt","hola en archivo txt creado", (err)=>{
+    if (err) {
+        console.log(err);        
+    }
+    console.log("Archivo creado");
+    
+}); */
+
+
+
+console.log
+console.log(" --------- ---------- ----------- ----------- ----------");
+
+
+
+
 
 console.log("Activo Script.js");
 
@@ -59,6 +115,7 @@ const data="la data aportada"; */
 
 
 import express from "express";
+import { arch, platform } from "os";
 import path from "path";
 import url from "url";
 
